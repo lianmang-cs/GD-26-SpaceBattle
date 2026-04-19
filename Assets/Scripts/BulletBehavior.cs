eeb07f03a5b551bda1e6577a83dc01625504d50d
lianmang-cs <lianmang238@gmail.com>
2026-04-19 10:48:44 -0500

    Re-added colliders for enemy and bullet
    For some reason, the colliders didn't save and I had to re add them and re tag the prefabs.

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -9,12 +9,12 @@ public class BulletBehavior : MonoBehaviour
    }
 
 
-   
+
    void OnTriggerEnter2D(Collider2D other) {
     //Destroy enemies tagged enemies
     if(other.CompareTag("Enemy")) {
-        Destroy(other.gameObject) //destroy enemies
-        Destroy(gameObject) //destroy bullet
+        Destroy(other.gameObject); //destroy enemies
+        Destroy(gameObject); //destroy bullet
     }
     //Destroy bullet if it hits the top
     if(other.CompareTag("TopBorder")) {
