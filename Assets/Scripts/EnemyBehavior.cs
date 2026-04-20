4193fe848f609f49eafc10f72a141e962c70a1a6
lianmang-cs <lianmang238@gmail.com>
2026-04-20 16:27:35 -0500

    Made the enemy group move left and right.
    The enemy group now moves left to right, making it more difficult to hit them.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    private float direction = 1f; 
+    public float moveSpeed = 1f; 
+    public float max;
+    public float min; 
+    
     public float minRateOfFire = 1f; 
     public float maxRateOfFire = 6f; 
     private float delay = 0f; 
@@ -10,6 +15,8 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement(); 
+
         timer += Time.deltaTime;
         if(timer >= delay) {
             randomShoot(); 
@@ -20,9 +27,25 @@ public class EnemyBehavior : MonoBehaviour
     public void randomShoot() {
         //get all enemies within the group
         EnemyLaunchOff[] enemies = GetComponentsInChildren<EnemyLaunchOff>(); 
-        int random = Random.Range(1, enemies.Length);
+        if(enemies.Length == 0) return; //stop if no more enemies 
+        int random = Random.Range(0, enemies.Length);
         //pick random enemy
         enemies[random].Shoot(); 
         
+    }
+    public void Movement() {
+        Vector3 newPos = transform.position;
+        newPos.x = newPos.x + direction * moveSpeed * Time.deltaTime; 
+        //prevent movement to far right
+        if (newPos.x > max) {
+            newPos.x = max;
+            direction = -1f; //flips direction  
+        }  
+        //prevent movement to far left
+        if (newPos.x < min) {
+            newPos.x = min; 
+            direction = 1f; //flips direction
+        }
+        transform.position = newPos; 
     }
 }
