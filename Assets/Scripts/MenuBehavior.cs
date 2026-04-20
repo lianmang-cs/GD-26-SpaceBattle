cb2409caf69852db962f3c5df7aaee459985c0d5
lianmang-cs <lianmang238@gmail.com>
2026-04-19 19:53:19 -0500

    Created a wave cleared panel
    The panel will appear when all the enemies are gone. For now, It has two buttons: continue and quit.

--- Assets/Scripts/MenuBehavior.cs
+++ Assets/Scripts/MenuBehavior.cs
@@ -2,10 +2,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuBehaviour : MonoBehaviour
 {
+    public GameObject enemyGroup;
+    public GameObject waveClearPanel;  
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //check if all enemies are gone
+        if (enemyGroup != null && enemyGroup.transform.childCount == 0) {
+            //disable the player
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<PlayerBehavior>().enabled = false; 
+            //display wave cleared panel
+            waveClearPanel.SetActive(true); 
+        }   
+    }
+    //continue the game
+    public void continueGame() {
+        //hide wave cleared panel
+        waveClearPanel.SetActive(false);    
+    }
+    //return to main menu
+    public void quitGame() {
+        SceneManager.LoadScene("MainMenu");
+    }
     public void goToGame() {
         SceneManager.LoadScene("GameScene"); 
-    }
-    public void goToMenu() {
-        SceneManager.LoadScene("MainMenu");
     }
 }
